0548905574cf39b1fddeb97e4f4b3701c8fe6284
joaopedrobrb <joaopedro.brb@gmail.com>
2023-05-15 15:25:57 -0500

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Atualizar o pip para a versão mais recente
+pip install --upgrade pip
+
+# Instalar os pacotes a partir do arquivo requirements.txt
+pip install -r ./requirements.txt
